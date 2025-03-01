46ef8b1c61f015a39abd156afd93a65e3a7bb209
RamanaR-2006 <ramanaramachandran14@gmail.com>
2025-03-01 03:10:17 -0600

    Allows lesson plan json to be displayed onto website

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import _ from 'lodash';
 
 function App() {
 
   const [data, setData] = useState([{}])
+
+  const [spanish_data, spanish_setData] = useState(null)
 
   useEffect(() => {
     fetch("/languages").then(
@@ -15,19 +18,69 @@ function App() {
     )
   }, [])
 
-  return (
-    <div>
+  useEffect(() => {
+    fetch("/spanish").then(
+      res => res.json()
+    ).then(
+      spanish_data => {
+        spanish_setData(spanish_data)
+        console.log(spanish_data)
+      }
+    )
+  }, [])
 
-        {(typeof data.languages === 'undefined') ? (
+
+  // function to convert lesson plan json into presentable data
+  const renderData = (data) => {
+    if (_.isArray(data)) {
+      return (
+        <ul>
+          {data.map((item, i) => (
+            <li key={i}>{item}</li>
+          ))}
+        </ul>
+      );
+    }
+
+    if (_.isObject(data)) {
+      return (
+        <ul>
+          {_.map(data, (value, key) => (
+            <li key={key}>
+              <strong>{key}:</strong> {renderData(value)}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+
+    return <span>{data}</span>;
+  };
+
+  return (
+    <>
+      <div>
+
+          {(typeof data.languages === 'undefined') ? (
+            <p>Loading...</p>
+          ) : (
+              data.languages.map((language, i) => (
+                  <p key={i}>{language}</p>
+              ))
+
+          )}
+
+      </div>
+
+      <div>
+        <h2>Lesson Plan</h2>
+        {!data ? (
           <p>Loading...</p>
         ) : (
-            data.languages.map((language, i) => (
-                <p key={i}>{language}</p>
-            ))
-
+          <div>{renderData(_.get(spanish_data, 'lesson_plan'))}</div>
         )}
-
-    </div>
+      </div>
+  </>
   )
 }
 
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route("/languages")
 def languages():
     return {"languages": ["English", "Spanish", "Dutch"]}
+
+@app.route("/spanish")
+def spanish():
+    return send_file('spanish.json', as_attachment=True, mimetype='application/json')
 
 if __name__ == "__main__":
     app.run(debug=True)
